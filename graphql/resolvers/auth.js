1da9e0c0426a133a69aff056e3902c3fbab87909
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-05-02 09:32:31 -0300

    Generating token - auth

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const User = require('../../models/user');
+
 
 module.exports = {
     createUser: async args => {
@@ -25,4 +28,18 @@ module.exports = {
             throw err;
         }
     },
+    login: async({email, password}) => {
+        const user = await User.findOne({email: email});
+        if (!user){
+            throw new Error('User does not exist!');            
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual){
+            throw new Error('Password is incorrect');
+        }
+        const token = jwt.sign({userId: user.id, email: user.email}, 'somesupersecretkey', {
+            expiresIn: '1h'
+        });
+        return { userId: user.id, token: token, tokenExpiration: 1 };
+    }
 }
