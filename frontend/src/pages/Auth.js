11a26b86a3f7b672185bbecb1a9ec769ef6410ad
fabio.camillo@3xsolutions.com.br <fabio.camillo@3xsolutions.com.br>
2019-07-18 09:48:03 -0300

    Auth page, logic login and create a user (signin and signup)

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,95 @@
 import React, { Component } from 'react';
 
+import './Auth.css';
+
 class AuthPage extends Component {
+  state = {
+    isLogin: true,
+  };
+  
+  constructor(props) {
+    super(props)
+    this.emailElement = React.createRef();
+    this.passwordElement = React.createRef();
+  }
+
+  switchModeHandler = () => {
+    this.setState(prevState => {
+      return { isLogin: !prevState.isLogin }
+    })
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+    const email = this.emailElement.current.value;
+    const password = this.passwordElement.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }   
+      `
+    };
+
+    if (!this.state.isLogin) {
+      requestBody = {
+        query: `
+        mutation {
+          createUser(userInput: {email: "${email}", password: "${password}"}) {
+            _id
+            email
+          }
+        }
+      `
+      };
+    }
+
+    fetch('http://localhost:2469/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      if(res.status !== 200 && res.status !== 201) {
+        throw new Error('Failed');
+      }
+      return res.json();
+    })
+    .then(resData => {
+      console.log(resData);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
   render() {
-    return <h1>The Auth Page</h1>;
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" ref={this.emailElement}/>
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordElement}/>
+        </div>
+        <div className="form-actions">
+          <button type="submit" id="email">{this.state.isLogin ? 'Login' : 'Create user'}</button>
+          <button type="button" onClick={this.switchModeHandler}>{this.state.isLogin ? 'Signup' : 'Signin'}</button>
+        </div>
+      </form>
+    );
   }
 }
 
